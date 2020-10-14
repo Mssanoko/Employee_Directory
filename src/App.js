6342724c53c7d2c700b07c5fbd9414dd49d10c6e
mssanoko <lawsonjleslie@gmail.com>
2020-10-13 20:27:44 -0400

    API.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import Main from './components/Main';
 import Wrapper from './components/Wrapper';
 import Header from './components/Header';
-import "./App.css";
+import Main from './components/Main';
 
 function App() {
   return (
-    <div className="App">
-   <Wrapper>
-     <Header />
-     <Main />
-   </Wrapper>
+    <div>
+      <Wrapper>
+        <Header />
+        <Main />
+      </Wrapper>
     </div>
   );
 }
