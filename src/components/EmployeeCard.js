a66e88fc8e4f3936b147f7d84d643b1df4c8a153
mssanoko <lawsonjleslie@gmail.com>
2020-10-13 20:05:12 -0400

    employee card

---
+++ src/components/EmployeeCard.js
@@ -0,0 +1,24 @@
+
+import React from "react";
+
+function EmployeeCard(props) {
+  return (
+    // <div className="row">
+    <tr>
+  
+      <th scope="row"><img alt={props.firstName}  src={props.picture} /></th>  
+      
+      <td >{props.firstName}</td>
+   
+      <td >{props.lastName}</td>
+    
+      <td >{props.email}</td>
+      
+      <td >{props.phone}</td>
+      
+   </tr>
+  
+  );
+}
+
+export default EmployeeCard;
