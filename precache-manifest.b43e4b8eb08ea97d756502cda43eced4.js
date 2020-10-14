1495056a4e4c2253c0cac85ccfdc85b5c47085bc
mssanoko <lawsonjleslie@gmail.com>
2020-10-14 19:51:48 -0400

    Updates

---
+++ precache-manifest.b43e4b8eb08ea97d756502cda43eced4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb89ddda96d4c8a47b9c4d7d520ed681",
+    "url": "/Employee_Directory_19/index.html"
+  },
+  {
+    "revision": "b54b80faf9b207d6366e",
+    "url": "/Employee_Directory_19/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fd05357e0e6db497cc37",
+    "url": "/Employee_Directory_19/static/js/2.0398a87e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Employee_Directory_19/static/js/2.0398a87e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b54b80faf9b207d6366e",
+    "url": "/Employee_Directory_19/static/js/main.adea4b43.chunk.js"
+  },
+  {
+    "revision": "7086336cb117ddec26d3",
+    "url": "/Employee_Directory_19/static/js/runtime-main.ee705bf1.js"
+  }
+]);
