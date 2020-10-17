10c3d9802920960edd3050312c3f48395f02fd0f
mssanoko <lawsonjleslie@gmail.com>
2020-10-17 12:01:41 -0400

    header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,22 @@ import React, { Component } from 'react';
 export default class Header extends Component {
     render() {
         return (
-            <div className="header">
-                <h1>Employee Directory</h1>
+            <div 
+            header style = {
+                styles.headerStyle
+            } className="header">
+                <h1 style = {styles.headingStyle}>Employee Directory</h1>
                 <h6>Start typing the name, phone number, or email of the employee you're looking for below</h6>
             </div>
         )
+        
+    }
+}
+const styles = {
+    headerStyle: {
+        background: "#d8bfd8"
+    },
+    headingStyle: {
+        fontSize: 50
     }
 }
